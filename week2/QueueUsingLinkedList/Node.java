5b7d9538fdbba2e4509f3e31c6eb53f3fde84572
Che Wei Kuo <000461843@student.vcc.ca>
2023-01-11 10:17:18 -0800

    week2 QueueUsingLinkedList and Array

---
+++ week2/QueueUsingLinkedList/Node.java
@@ -0,0 +1,11 @@
+package QueueUsingLinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node() {
+        data = 0;
+        next = null;
+    }
+}
---
+++ week2/QueueUsingLinkedList/Queue.java
@@ -0,0 +1,91 @@
+package QueueUsingLinkedList;
+
+import javax.xml.transform.Source;
+
+class Queue {
+
+    Node front;
+    int size;
+  
+    Queue() {
+      front = null;
+      size = 0;
+    }
+  
+    void enqueue(int input) {
+      Node aNewNode = new Node();
+      aNewNode.data = input;
+  
+      if (front == null)
+        front = aNewNode;
+      else {
+        // the queue is not empty
+        Node tmp = front;
+        while (tmp != null) {
+          if (tmp.next == null) {
+            break;
+          }
+          tmp = tmp.next;
+
+        }
+        tmp.next = aNewNode;
+
+      }
+      size += 1;
+
+    }
+  
+    void dequeue() {
+      if (front != null) {
+        front = front.next;
+        size -= 1;    
+        return; 
+      }
+
+    }
+
+    void getFront(){
+        if (front == null) {
+            return;
+        }
+        System.out.println(front.data);
+       
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    boolean isEmpty() {
+        if(front == null)
+            return true;
+
+        return false;    
+    }
+
+    boolean contains(int input) {
+        Node tmp = front;
+        while (tmp != null) {
+            if (tmp.data == input) {
+                return true;
+            }
+            tmp = tmp.next;
+        }
+        return false;
+    }
+    
+  
+    public static void main(String[] args) {
+      Queue q = new Queue();
+      q.enqueue(1); // add 1 to the back of the queue
+      q.enqueue(2);
+      q.enqueue(3);
+      q.dequeue(); // remove 1 from the fron the queue
+      q.dequeue(); // remove 2
+      q.getFront();  // 3
+      System.out.println(q.getSize());//1
+      System.out.println(q.isEmpty());// false
+      System.out.println(q.contains(1));// false
+     
+    }
+  }
