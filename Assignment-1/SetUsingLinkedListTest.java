5b78039e6bc9714dc0683338a414cfb82f041810
Bill Kuo <billkuo08@gmail.com>
2023-01-30 19:11:52 -0800

    add some tests

--- Assignment-1/SetUsingLinkedListTest.java
+++ Assignment-1/SetUsingLinkedListTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -13,6 +14,8 @@ public class SetUsingLinkedListTest {
         mySet.add(3);
         mySet.add(4);
         mySet.add(4);//should be ignored since duplicate
+        assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, mySet.toArray());
+
         assertEquals(4, mySet.getCurrentSize());//4
         assertEquals(true, mySet.contains(4));
         mySet.remove(3);
