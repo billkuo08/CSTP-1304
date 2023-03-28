95e9e96b6d00a13ee108e7ab7a535ca92c4fcd7d
Bill Kuo <billkuo08@gmail.com>
2023-03-28 14:30:39 -0700

    fixed heap and added heapSort

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -18,26 +18,65 @@ class Heap {
         arr[11] = 21;
         arr[12] = 17;
         lastIndex = 12;
-
     }
 
     void insert(int newElement) {
-        lastIndex++;
+        ++lastIndex;
         arr[lastIndex] = newElement;
         bubbleUp(lastIndex);
+    }
+
+    // bubble up the element at the provided index
+    void bubbleUp(int elementIndex) {
+        int theElementToBeBubbledUp = arr[elementIndex];
+        int indexOfTheElementToBeBubbledUp = elementIndex;
+        // check whether we need to do swap
+        // parent of 81
+        int parentIndex = (elementIndex - 1) / 2;
+        int parentElement = arr[parentIndex];
+        while (parentElement < theElementToBeBubbledUp) {
+            swap(parentIndex, indexOfTheElementToBeBubbledUp);
+            indexOfTheElementToBeBubbledUp = parentIndex;
+            parentIndex = (parentIndex - 1) / 2;
+            if (parentElement < 0) {
+                break;
+            }
+            parentElement = arr[parentIndex];
+        }
 
     }
 
-    // bubble down the element at the provdied index
+    // remove the root from the heap
+    int remove() {
+        int result = arr[0];
+        arr[0] = arr[lastIndex];
+        lastIndex--;
+        if(lastIndex != 0)
+            bubbleDown(0);
+        return result;
+
+    }
+
+    void swap(int aIndex, int b) {
+        int tmp = arr[aIndex];
+        arr[aIndex] = arr[b];
+        arr[b] = tmp;
+    }
+
+    // bubble down the element at the provided index
     void bubbleDown(int index) {
         int i = index;
+
         int leftChildIndex = 2 * i + 1;
         int rightChildIndex = 2 * i + 2;
+        int x = arr[i];
         int leftChildElement = arr[2 * i + 1];
         int rightChildElement = arr[2 * i + 2];
-        int maxIndex = i;
-        while (maxIndex < lastIndex) {
-            // while (leftChildElement > arr[i] || rightChildElement > arr[i]) {
+
+
+        while (leftChildElement >= x ||
+                rightChildElement >= x) {
+            int maxIndex;
             if (leftChildElement > rightChildElement) {
                 maxIndex = leftChildIndex;
             } else {
@@ -50,59 +89,41 @@ class Heap {
 
             leftChildIndex = 2 * i + 1;
             rightChildIndex = 2 * i + 2;
-
             if (leftChildIndex > lastIndex || rightChildIndex > lastIndex)
                 break;
-
             leftChildElement = arr[2 * i + 1];
             rightChildElement = arr[2 * i + 2];
         }
+
     }
 
-    // bubble up the element at the provided index
-    void bubbleUp(int elementIndex) {
-        int elementToBubbleUp = arr[elementIndex];
-        int indexOfElementToBubbleUp = elementIndex;
-        // check whether we need to swap
-        // parent of 81
-        int parentIndex = (elementIndex - 1) / 2;
-        int parentElement = arr[parentIndex];
-
-        while (parentElement < elementToBubbleUp) {
-            swap(parentElement, indexOfElementToBubbleUp);
-            indexOfElementToBubbleUp = parentIndex;
-            parentIndex = (parentIndex - 1) / 2;
-
-            if (parentIndex < 0)
-                break;
+    void heapSort() {
+        // Heapify the array
+        for (int i = (lastIndex / 2) - 1; i >= 0; i--) {
+            bubbleDown(i);
         }
-        parentIndex = arr[parentIndex];
-    }
 
-    // remove the root from the heap
-    void remove() {
-        arr[0] = arr[lastIndex];
-        lastIndex--;
-        bubbleDown(0);
-
-    }
-
-    void swap(int aIndex, int bIndex) {
-        int tmp = arr[aIndex];
-        arr[aIndex] = arr[bIndex];
-        arr[bIndex] = tmp;
-
+        // remove the root and add it to the end of the array
+        // remove the prcess until the heap is empty
+        int size = lastIndex + 1;
+        for (int i = 0; i < size; i++) {
+            System.out.println(remove());
+        }
     }
 
     public static void main(String[] args) {
-        //test case icls insertion
+        // test case 1 insertion
         Heap aHeapTree = new Heap();
-        // aHeapTree.insert(81); // { 98, 86, 81, 13, 65, 32, 41, 9, 10, 44, 23, 21, 17,
-        // 29 }; // Expected Heap
-        // // assert the new size - 14
+        // aHeapTree.insert(81); // { 98, 86, 81, 13, 65, 32, 41, 9, 10, 44, 23, 21, 17
+        // } // Expected Heap, new size 13
 
         // test case 2 removal
-        aHeapTree.remove();// { 86, 65, 41, 13, 44, 32, 29, 9, 10, 17, 23, 21}; // Expected Heap
+        // aHeapTree.remove();// { 86, 65, 41, 13, 44, 32, 29, 9, 10, 17, 23, 21}; //
+        // Expected Heap
+
+        // new size 12
+        aHeapTree.heapSort();// 98, 86, 81, 65, 44, 41, 32, 29, 23, 21, 17 ,13, 10, 9
 
     }
+
 }
