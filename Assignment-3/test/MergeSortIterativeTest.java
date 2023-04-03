58524643edd3d8d2e208ddd276f893cad8aeff8a
Bill Kuo <billkuo08@gmail.com>
2023-04-02 23:19:34 -0700

    MergeSortIterative Test, added

--- Assignment-3/test/MergeSortIterativeTest.java
+++ Assignment-3/test/MergeSortIterativeTest.java
@@ -2,16 +2,15 @@ package test;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import src.MergeSortRecursive;
+import src.MergeSortIterative;
 
-public class MergeSortRecursiveTest {
-
+public class MergeSortIterativeTest {
     @Test
     public void evenSizeTest() {
         int[] arr = { 5, 1, 6, 2, 3, 4 };
         int[] expected = { 1, 2, 3, 4, 5, 6 };
         int n = arr.length;
-        MergeSortRecursive myMergeSort = new MergeSortRecursive();
+        MergeSortIterative myMergeSort = new MergeSortIterative();
         myMergeSort.mergeSort(arr, n);
         assertArrayEquals(expected, arr);
     }
@@ -21,7 +20,7 @@ public class MergeSortRecursiveTest {
         int[] arr = { 12, 11, 13, 5, 0 };
         int[] expected = { 0, 5, 11, 12, 13 };
         int n = arr.length;
-        MergeSortRecursive myMergeSort = new MergeSortRecursive();
+        MergeSortIterative myMergeSort = new MergeSortIterative();
         myMergeSort.mergeSort(arr, n);
         assertArrayEquals(expected, arr);
     }
@@ -31,7 +30,7 @@ public class MergeSortRecursiveTest {
         int[] arr = { 5, 1, 6, 2, 3, -4 };
         int[] expected = { -4, 1, 2, 3, 5, 6 };
         int n = arr.length;
-        MergeSortRecursive myMergeSort = new MergeSortRecursive();
+        MergeSortIterative myMergeSort = new MergeSortIterative();
         myMergeSort.mergeSort(arr, n);
         assertArrayEquals(expected, arr);
     }  
@@ -41,7 +40,7 @@ public class MergeSortRecursiveTest {
         int[] arr = { 12, 11, 13, 5, 11 };
         int[] expected = { 5, 11, 11, 12, 13 };
         int n = arr.length;
-        MergeSortRecursive myMergeSort = new MergeSortRecursive();
+        MergeSortIterative myMergeSort = new MergeSortIterative();
         myMergeSort.mergeSort(arr, n);
         assertArrayEquals(expected, arr);
     }
